7bfd9e5d84cd01eef0e42f97fb91449ca93771ec
Roman Smolka <roman.smolka@me.com>
2019-01-08 01:13:19 +0100

    init, v0.0.1

---
+++ README.md
@@ -0,0 +1,4 @@
+# Kodi Key Lock addon
+
+<img src="icon.png" alt="icon" width="200" height="200" style="float:left; margin-right: 1.5em"/>
+Disables remote key input when active by switching keymap.xml configurations.
---
+++ default.py
@@ -0,0 +1,70 @@
+import xbmc
+import xbmcgui
+import xbmcaddon
+import shutil
+import urlparse
+import sys
+
+# create a class for your addon, we need this to get info about your addon
+ADDON = xbmcaddon.Addon()
+PATH = ADDON.getAddonInfo('path').decode('utf-8')
+
+keymaps_dir = xbmc.translatePath("special://userdata/keymaps")
+
+def numbInput():
+	try:
+		shutil.move(keymaps_dir+'/gen.xml', keymaps_dir+'/gen.xml.disabled')
+		shutil.move(keymaps_dir+'/numb.xml.disabled', keymaps_dir+'/numb.xml')
+		xbmc.sleep(100)
+		xbmc.executebuiltin('Action(reloadkeymaps)')
+	except:
+		pass
+
+def restoreInput():
+	try:
+		shutil.move(keymaps_dir+'/gen.xml.disabled', keymaps_dir+'/gen.xml')
+		shutil.move(keymaps_dir+'/numb.xml', keymaps_dir+'/numb.xml.disabled')
+		xbmc.sleep(100)
+		xbmc.executebuiltin('Action(reloadkeymaps)')
+	except:
+		pass
+
+class GUI(xbmcgui.WindowXMLDialog):
+
+	def onInit(self):
+		xbmc.executebuiltin('Container.SetViewMode(50)')
+		self.clearList()
+
+		numbInput()
+		xbmc.sleep(100)
+		self.setFocusId(1)
+
+	def onAction(self, action):
+		if action.getId() == 10:
+			self.close()
+		if action.getId() == 2:
+			self.setFocusId(2)
+			self.triggered = True
+			xbmc.sleep(3000)
+			self.setFocusId(1)
+			self.triggered = False
+		if action.getId() == 7:
+			if self.triggered: 
+				restoreInput()
+				self.close()
+
+action = None
+if len(sys.argv) > 1: 
+	args = urlparse.parse_qs(sys.argv[2][1:])
+	action = args.get('action', None)
+
+if action == 'restore':
+	xbmc.log('Restoring keymap configuration gen.xml')
+	restoreInput()
+else:	
+	ui = GUI('main.xml', PATH, 'default', '720p')
+	ui.doModal()
+
+	restoreInput()
+
+	del ui
